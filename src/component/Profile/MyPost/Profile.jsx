6452bc52a5decacdc645679697d3771d6fd331e8
Misha Tkach <mtkach@e1r6p1.unit.ua>
2020-12-02 12:49:41 +0200

    React00

---
+++ src/component/Profile/MyPost/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Profile.module.css'
+
+function Profile() {
+    return (
+        <div className = { classes.content }>
+            <img src="https://football24.ua/resources/photos/news/1200x675_DIR/202011/634248.jpg?202011235838" />
+            <div>
+                ava + des
+    </div>
+            <div>
+                My post
+    </div>
+            <div>
+                New post
+    </div>
+            <div>
+                post
+    </div>
+        </div>
+    );
+}
+export default Profile;
